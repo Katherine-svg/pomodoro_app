8e63d60a8df8edc02d18f730fde311121fcfe1a6
space-almonte <almonte.space@gmail.com>
2020-06-08 13:23:26 -0400

    adding settings, login, signup, footer, landing, navbar components

--- frontend/pomodoro/src/app/app.module.ts
+++ frontend/pomodoro/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AccSettingsComponent } from './components/acc-settings/acc-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    AccSettingsComponent
   ],
   imports: [
     BrowserModule,
