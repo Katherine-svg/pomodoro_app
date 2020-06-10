5b13fd7bf6ffeb96df8c20adaac6cb6adef87264
space-almonte <almonte.space@gmail.com>
2020-06-10 15:45:40 -0400

    working on pomodoro clock

--- frontend/pomodoro/src/app/components/core-appservices/pomodoro-clock/pomodoro-clock.component.ts
+++ frontend/pomodoro/src/app/components/core-appservices/pomodoro-clock/pomodoro-clock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task, Intervals } from 'src/app/shared/models/model';
 
 @Component({
   selector: 'app-pomodoro-clock',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pomodoro-clock.component.css']
 })
 export class PomodoroClockComponent implements OnInit {
-// pomodoro clock variables
+
+  // task form variables
+  goal: string
+  task: Task
+  status: string
+  
+  // pomodoro clock variables
   time: number = 0
-  interval
+  interval: any
 
   secondsToMinutes: any = 0
 
@@ -20,10 +27,34 @@ export class PomodoroClockComponent implements OnInit {
   mString
 
   active: boolean = false
+/*
+// time stamps
+setTime: number
+// a pomodoro is 25 minutes ie 1500 sec
+pomodoro: number = 1500
 
-// task form variables
+// breaks under 4 pomodoros are 5 minutes
+shortBreak: number = 300
 
-goal: string
+// a break after 4 pomodoro is 30 minutes
+longBreak: number = 1800
+*/
+
+// time stamps
+setTime: number
+// a pomodoro is 25 minutes ie 1500 sec
+pomodoro: number = 4
+
+// breaks under 4 pomodoros are 5 minutes
+shortBreak: number = 3
+
+// a break after 4 pomodoro is 30 minutes
+longBreak: number = 5
+// tracking pomodoros
+
+pomodoroComplete: number = 0
+pomodoroCount: number = 1
+pomodoroTotal: number = 0
 
   constructor() { }
 
@@ -35,36 +66,100 @@ goal: string
 
   start() {
    this.interval = setInterval(() => {
+      this.timeCount(this.time)
       this.time++;
-
-      if (this.time < 60)
-      this.secondsToMinutes = this.time
-      else if (!(this.time > 1500))
-      this.secondsToMin(this.time)
-      else
-      this.reset()
     }, 1500)
-    this.active = true
   }
+
+  timeCount(time: number) {
+    if (this.pomodoroComplete == 0) {
+      
+      this.status =  "pomodoro number " + this.pomodoroCount + "."
+
+      this.active = true
+      this.secondsToMin(this.time)
+
+      if (time == this.pomodoro) {
+                  this.pomodoroCount++
+                  this.pomodoroComplete++
+                  this.resetTime("pomodoro", this.time)
+      }
+
+      console.log("pomodoro"+time)
+
+    }
+    else if (this.pomodoroCount <= 4 
+              && this.pomodoroComplete == 1) {
+      
+      this.status = "short break"
+
+      this.active = true
+      this.secondsToMin(this.time)
+
+      if (time == this.shortBreak) {
+                    this.pomodoroComplete = 0
+                    this.resetTime("short break", this.time)
+      }
+      
+      console.log("break"+time)
+      
+    }
+    else {
+      
+      this.status = "long break"
+
+      this.active = true
+      this.secondsToMin(this.time)
+
+      console.log("long break pomodoroCount"+this.pomodoroCount)
+
+      if (time == this.longBreak) {
+                    this.pomodoroTotal += this.pomodoroCount
+                    this.pomodoroCount = 1
+                    this.pomodoroComplete = 0
+                    this.resetTime("long break", this.time)
+                    console.log(this.task)
+      }
+      
+      console.log("long break"+time)
+      
+    }
+  }
+
   pause() {
     clearInterval(this.interval);
   }
 
+  resetTime(type, timeInSeconds) {
+
+    console.log(type +" "+ timeInSeconds)
+    console.log(new Intervals(type, new Date, timeInSeconds))
+
+    this.task.addPomodoro(
+      new Intervals(type, new Date, timeInSeconds)
+    )
+    this.reset()
+  }
+
   reset() {
     clearInterval(this.interval);
+    this.active = false
     this.time = 0
     this.secondsToMinutes = this.time
   }
 
   secondsToMin(s:number) {
 
-    this.hour = Math.floor(s / 3600)
+    // this.hour = Math.floor(s / 3600)
     this.minutes = Math.floor(s % 3600 / 60)
     this.seconds = Math.floor(s % 3600 % 60)
 
     this.sString = `0${this.seconds}`
-    this.mString =  `0${this.minutes}`
+    this.mString =  `${this.minutes}`
 
+    if (s < 60)
+    this.secondsToMinutes = s
+    else
     this.secondsToMinutes =  `${this.mString.slice(-2)} : ${this.sString.slice(-2)}`
 
   }
@@ -73,7 +168,7 @@ goal: string
 
   saveTask(f) {
     this.goal = f.value.task
-    console.log(this.goal)
+    this.task = new Task(this.goal, new Date)
   }
 
 }
--- frontend/pomodoro/src/app/shared/models/model.ts
+++ frontend/pomodoro/src/app/shared/models/model.ts
@@ -3,11 +3,56 @@ export enum Role {
     Admin = 'Admin'
   }
 
-  export class User {
+export class User {
     id: number;
     username: string;
     password: string;
     email: string;
     role: Role;
     token?: string;
+}
+
+export class Task {
+  task: string;
+  startTime: Date;
+  pomodoro: Intervals[]
+  endTime?: Date;
+  id?: number;
+
+  constructor(task: string
+    , startTime: Date
+    , pomodoro: Intervals[] = []
+    , id?: number ){
+
+    this.task = task;
+    this.startTime = startTime;
+    this.pomodoro = pomodoro;
+    this.id = id;
+  }
+
+  addPomodoro(time: Intervals) {
+    this.pomodoro.push(time);
+  }
+
+  getPomodoro() {
+   return this.pomodoro;
+  }
+}
+
+export class Intervals {
+  type: string;
+  startTime: Date;
+  timeInSeconds: number;
+  id?: number;
+
+  constructor(type: string
+    , startTime: Date
+    , timeInSeconds: number
+    , id?: number ){
+
+    this.type = type;
+    this.startTime = startTime;
+    this.timeInSeconds = timeInSeconds;
+    this.id = id;
+  }
 }
